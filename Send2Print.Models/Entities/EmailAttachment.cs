9995de8bf2a9db0772b46a2b694c461e45f77083
F-Barszczewski <fbarszczewski@outlook.com>
2024-06-28 22:04:51 +0200

    Created base email entities

---
+++ Send2Print.Models/Entities/EmailAttachment.cs
@@ -0,0 +1,24 @@
+﻿namespace Send2Print.Models.Entities;
+
+public class EmailAttachment
+{
+    public required string FileName { get; init; } = string.Empty;
+    public required string FilePath { get; init; } = string.Empty;
+    public long Size { get; set; }
+    public string FileType => _fileType.Value;
+    private readonly Lazy<string> _fileType;
+
+    public EmailAttachment()
+    {
+        _fileType = new Lazy<string>(() => Path.GetExtension(FilePath).TrimStart('.').ToLower());
+    }
+
+    private static string DetermineFileType(string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath)) return "";
+
+        var extension = Path.GetExtension(filePath).TrimStart('.').ToLower();
+
+        return string.IsNullOrEmpty(extension) ? "" : extension;
+    }
+}
---
+++ Send2Print.Models/Entities/MailEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Send2Print.Models.Entities;
+
+public class MailEntity
+{
+    public uint Id { get; set; }
+    public string Subject { get; set; } = string.Empty;
+    public string From { get; set; } = string.Empty;
+    public List<string> To { get; set; } = [];
+    public List<string> Cc { get; set; } = [];
+    public string Body { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public List<EmailAttachment> Attachments { get; set; } = [];
+}
